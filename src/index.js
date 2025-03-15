5235ca28b47945817fa688c0022e73db3331fcbb
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-03-15 22:41:58 +0300

    Index Added: settings.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") return; 
+
+    
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    
+    taskList.appendChild(li);
+
+    
+    form.reset();
+  });
 });
